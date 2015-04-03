25d1c1c32a880109635aef78a77d15ea5aed4a43
quattromani <max@maxquattromani.com>
2015-04-03 15:56:57 -0600

    Updates

--- src/js/banner.js
+++
@@ -1,6 +0,0 @@
-// $(function() {
-//   var headerHeight = $('.header-banner').outerHeight();
-//   var navHeight = $('nav').outerHeight();
-//   $('.banner').css('margin-top', -(headerHeight + navHeight));
-//   console.log(headerHeight);
-// });
