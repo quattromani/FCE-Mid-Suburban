51e76e7f97d4904252711b0a4f861a29216e9d1d
quattromani <max@maxquattromani.com>
2015-04-05 01:02:04 -0600

    Updates

--- js/build/production.js
+++ js/build/production.js
@@ -54,11 +54,29 @@ $('.fonts').each(function(){
 ;$(function() {
   var headerHeight = $('.header-banner').outerHeight();
   var navHeight = $('nav').outerHeight();
-  $('.banner').css('margin-top', -(headerHeight + navHeight));
+  $('.banner').css('margin-top', - (headerHeight + navHeight));
   console.log(headerHeight);
 });
 ;// Open all external links in a new window
 $('a[href^="http://"], a[href^="https://"], a[href$=".pdf"]').attr('target','_blank');
+;$(function(){
+
+	$(this).keyup(function(event) {
+		if (event.which === 27) {
+			disablePopup();
+		}
+	});
+
+	$('.blur-overlay').click(function() {
+		disablePopup();
+		return false;
+	});
+});
+
+function disablePopup() {
+	$('.modal-container').fadeOut('normal');
+	$('.blur-overlay').fadeOut('normal');
+}
 ;/*
 Syntax highlighting with language autodetection.
 https://highlightjs.org/
@@ -805,6 +823,70 @@ $(document).ready(function() {
   });
 });
 
+;(function( $ ) {
+
+  $.fn.PieMenu = function(options) {
+	var angle,
+		delay_time,
+		ele_angle=[],
+		x_pos=[],
+		y_pos=[];
+
+    var settings = $.extend( {
+      'starting_angel'   : '0',
+      'angel_difference' : '90',
+	  'radius':'200',
+	  'menu_element' : this.children('.menu_option').children(),
+	  'menu_button' : this.children('.menu_button'),
+    }, options);
+
+
+	angle = parseInt(settings.angel_difference)/(settings.menu_element.length-1);
+	delay_time = 1/(settings.menu_element.length-1);
+
+	function setPosition(val){
+		$(settings.menu_element).each(function(i,ele){
+			$(ele).css({
+			'left' : (val==0)?0:y_pos[i],
+			'top' : (val==0)?0:-x_pos[i],
+			});
+		});
+	}
+
+	$(settings.menu_button).unbind('click', clickHandler);	//remove event if exist
+
+	var clickHandler = function() {
+		if($(this).parent().hasClass('active')){
+			setPosition(0);
+			$(this).parent().removeClass('active');
+			$(this).parent().addClass('inactive');
+
+		}else{
+			setPosition(1);
+			$(this).parent().addClass('active');
+			$(this).parent().removeClass('inactive');
+		}
+		$(this).toggleClass("btn-rotate");
+	};
+
+	$(settings.menu_button).bind('click', clickHandler);
+
+	return settings.menu_element.each(function(i,ele){
+		ele_angle[i] = (parseInt(settings.starting_angel) + angle*(i))*Math.PI/180;
+		 x_pos[i] = (settings.radius * Math.sin(ele_angle[i]));
+         y_pos[i] = (settings.radius * Math.cos(ele_angle[i]));
+
+		 $(ele).css({
+			'-webkit-transform': 'rotate('+(90-ele_angle[i]*180/Math.PI)+'deg)',
+			   '-moz-transform': 'rotate('+(90-ele_angle[i]*180/Math.PI)+'deg)',
+			    '-ms-transform': 'rotate('+(90-ele_angle[i]*180/Math.PI)+'deg)',
+			     '-o-transform': 'rotate('+(90-ele_angle[i]*180/Math.PI)+'deg)',
+			    	'transform': 'rotate('+(90-ele_angle[i]*180/Math.PI)+'deg)',
+		});
+      })
+
+  };
+})( jQuery );
 ;// Add classes to first and last li's for every instance
 $(function() {
   // Add classes to first and last of each list
