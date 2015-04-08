66539bf835ec52bc7afb51ec30b0e013b17dc931
quattromani <max@maxquattromani.com>
2015-04-07 22:09:00 -0600

    updating master

--- dist/js/build/production.js
+++ dist/js/build/production.js
@@ -75,7 +75,7 @@ $(window).resize(function() {
 	}
 });
 ;// Open all external links in a new window
-$('a[href^="http://"], a[href^="https://"], a[href$=".pdf"]').attr('target','_blank');
+$('a[href^="http://"], a[href^="https://"], a[href$=".pdf"], a:not([href^=mailto]').attr('target','_blank');
 ;$(function(){
 
 	$(this).keyup(function(event) {
--- src/js/external-links.js
+++ src/js/external-links.js
@@ -1,2 +1,2 @@
 // Open all external links in a new window
-$('a[href^="http://"], a[href^="https://"], a[href$=".pdf"]').attr('target','_blank');
+$('a[href^="http://"], a[href^="https://"], a[href$=".pdf"], a:not([href^=mailto]').attr('target','_blank');
